9bc91278c404cfb9ef9c363b88a4f4ade1adacad
xmuncam88 <165704566+xmuncam88@users.noreply.github.com>
2025-03-29 20:14:54 +0100

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,3 +1,3 @@
 log.debug('running', 'running')
-const id = record.load({ type: 'salesorder', id: 654321 })
-log.debug('loaded', id)
+const soId = record.load({ type: 'salesorder', id: 16479 })
+log.debug('loaded', soId)
